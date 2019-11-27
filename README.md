aa634721bdcfb541723398fe57df6bae2dea5a59
Daniel Lutz <daniel.lutz@outlook.com>
2019-11-27 19:38:14 +0100

    code init

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ license:    MIT
 
 ## Description
 
+This add-on checks if desktop projects are still available when you start Citavi and deletes them from the list of recently opened projects if it necessary.
+
 ## Disclaimer
 
 >There are no support claims by the company **Swiss Academic Software GmbH**, the provider of **Citavi** or other liability claims for problems or data loss. Any use is at your own risk. All rights to the name **Citavi** and any logos used are owned by **Swiss Academic Software GmbH**.
---
+++ src/KnownProjectsCleaner/Addon.cs
@@ -0,0 +1,31 @@
+﻿using SwissAcademic.Citavi;
+using SwissAcademic.Citavi.Shell;
+using System;
+
+namespace KnownProjectsCleaner
+{
+    public class Addon : CitaviAddOn<MainForm>
+    {
+        #region Constructors
+
+        public Addon() => CleanKnownProjectCollections();
+
+        #endregion
+
+        #region Methods
+
+        void CleanKnownProjectCollections()
+        {
+            for (int i = Program.Engine.Settings.General.KnownProjects.Count-1; i >= 0; i--)
+            {
+                var knownProject = Program.Engine.Settings.General.KnownProjects[i];
+                if (knownProject.ProjectType != ProjectType.DesktopSQLite) continue;
+                if(knownProject.ConnectionString.EndsWith(".ctv6", StringComparison.OrdinalIgnoreCase) && !System.IO.File.Exists(knownProject.ConnectionString))
+                    Program.Engine.Settings.General.KnownProjects.Remove(knownProject);
+            }
+        }
+
+        #endregion
+
+    }
+}
